9748831ce6cb80f37f2f999b248ee2cb2597a0a7
Matthew Carson <matthew24carson@gmail.com>
2019-09-23 17:11:12 -0400

    Tweaks

--- 2-hero/js/scripts.js
+++ 2-hero/js/scripts.js
@@ -1,7 +1,5 @@
 const heroDiv = document.querySelector('.hero');
 
-heroDiv.style.color = "#000";
-
 document.addEventListener('DOMContentLoaded', () => {
 	heroDiv.style.color = "#FFF";
 	heroDiv.classList.add('image');
